d304dfbd05fdb7ae1a648ba9ec100dda12c65c4e
Solomon Girma <92579234+like-github1@users.noreply.github.com>
2021-11-22 23:55:10 -0800

    Container_widget

--- lib/Screens/first_screen.dart
+++ lib/Screens/first_screen.dart
@@ -1,29 +1,46 @@
-import 'dart:math';
 
 import 'package:flutter/material.dart';
 
-class firstscreen extends StatelessWidget {
+class  Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return  Material(
-      color: Colors.blue,
-      child: Center(
-        child: Text(
-          GenerateRandomNumber(),
-          textDirection: TextDirection.ltr,
-          style: const TextStyle(
-            color: Colors.amber,
-            fontSize: 23,
-            fontWeight: FontWeight.bold,
-          ),
+    return Center(
+      child: Container(
+        width: 200.0,
+        height: 100.0,
+        alignment: Alignment.center,
+        color: Colors.deepPurple,
+        child: const Text(
+            "free Containers",
+             textDirection: TextDirection.ltr,
+           style: TextStyle(
+             color: Colors.amber,
+             fontSize: 18,
+             fontWeight: FontWeight.bold,
+           ),
+
+
         ),
       ),
     );
   }
-
-  String GenerateRandomNumber() {
-    var randomNum = Random();
-    int uniqueNumber = randomNum.nextInt(10);
-    return "your Lucky Number is $uniqueNumber";
-  }
+//function that Generates Random NuMBER
+  // String GenerateRandomNumber() {
+  //   var randomNum = Random();
+  //   int uniqueNumber = randomNum.nextInt(10);
+  //   return "your Lucky Number is $uniqueNumber";
+  // }
 }
+//in these section we will learn
+//row and collomun widget
+//text
+//Scafold and appbar
+//RaisedButton
+//AlertDialog box
+//Box cONSTRAINTS
+//container
+//images
+//size , margin , padding, ListView, and Floating action button
+//appbar
--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,10 @@
-import 'dart:math';
-
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
 
 import 'Screens/first_screen.dart';
+void main(){
 
-//run app method atach the code to the screen
-void main()=> runApp(const Myapp());
-
-
-class Myapp extends StatelessWidget {
-  const Myapp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-        title: "new flutter app",
-        home:Scaffold(
-          appBar: AppBar(
-            title: const Text("hello Flutter"),
-            centerTitle: true,
-            automaticallyImplyLeading: true,
-          ),
-          body: firstscreen(),
-
-        )
-    );
-  }
-  //functons That Generated a Random Number
-
+  runApp(MaterialApp(
+    title: "Exploring Flutter widget",
+    home: Home(),
+  ));
 }
-
